cfe745f4b9eaf216c5378cde288b27f97b0de826
Mimi Ke <mimi.g.ke@gmail.com>
2021-03-29 21:02:48 -0500

    shortened url length

--- backend/db/seeders/20210329222224-bathroom-data.js
+++ backend/db/seeders/20210329222224-bathroom-data.js
@@ -14,7 +14,7 @@ module.exports = {
          state: "WI",
          costPerDay: 40.00,
          locationType: "public bathroom",
-         bathroomImgUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.forbes.com%2Fsites%2Fjamiegold%2F2021%2F01%2F12%2Feight-residential-bathroom-design-trends-for-2021%2F&psig=AOvVaw1Z95_eEA3_E25dkOUBkqNK&ust=1617144240301000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCMmvLJ1u8CFQAAAAAdAAAAABAG"
+         bathroomImgUrl: "https://www.google.com/url?sa=i&u"
 
        }
      ], {});
