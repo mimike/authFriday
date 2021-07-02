7919496ec5adb7a0423ec22025dedc60a35c319b
Mimi Ke <mimi.g.ke@gmail.com>
2021-07-01 22:03:31 -0500

    form styling

--- frontend/src/components/Host/index.js
+++ frontend/src/components/Host/index.js
@@ -46,6 +46,59 @@ function Host() {
         }
     }, [bathroom.id]);
 
+    const states = [
+        "AL",
+        "AK",
+        "AZ",
+        "AR",
+        "CA",
+        "CO",
+        "CT",
+        "DE",
+        "FL",
+        "GA",
+        "HI",
+        "ID",
+        "IL",
+        "IN",
+        "IA",
+        "KS",
+        "KY",
+        "LA",
+        "ME",
+        "MD",
+        "MA",
+        "MI",
+        "MN",
+        "MS",
+        "MO",
+        "MT",
+        "NE",
+        "NV",
+        "NH",
+        "NJ",
+        "NM",
+        "NY",
+        "NC",
+        "ND",
+        "OH",
+        "OK",
+        "OR",
+        "PA",
+        "RI",
+        "SC",
+        "SD",
+        "TN",
+        "TX",
+        "UT",
+        "VT",
+        "VA",
+        "WA",
+        "WV",
+        "WI",
+        "WY",
+      ];
+
     return (
         <>
             <div className="host-title" style={{ color: "orange", marginLeft: "30px", marginTop: "40px" }}>
@@ -117,14 +170,30 @@ function Host() {
                     <div className="form-input">
 
                         <label>State</label>
-                        <input
+                        {/* <input
+                            className="state-dropdown"
                             style={{ width: "450px" }}
                             type="text"
                             value={state}
-
                             onChange={(e) => setState(e.target.value)}
                             required
-                        />
+                        /> */}
+                        <select
+                            className="state-dropdown"
+                            value={state}
+                            onChange={(e) => setState(e.target.value)}
+                            required
+                            >
+                            <option value={0} disabled>
+                                Select State
+                            </option>
+                            {states.map((state) => (
+                                <option type="text" value={state} key={state}>
+                                {state}
+                                </option>
+                            ))}
+                </select>
+
 
                     </div>
 
